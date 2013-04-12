8bb2f709638f39b8ea083862d121947f7e96de7b
Benjamin Wolf <benjamin.wolf@eledia.de>
2013-04-12 14:16:35 +0200

    eledia cut dependency to eledia_lib plugin and cleanup coding style

--- blocks/eledia_usercleanup/config_usercleanup.php
+++ blocks/eledia_usercleanup/config_usercleanup.php
@@ -1,33 +1,51 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-    require_once('../../config.php');
-    require_once('config_usercleanup_form.php');
+/**
+ * Configuration for the cleanup.
+ *
+ * @package    block
+ * @subpackage eledia_usercleanup
+ * @author     Benjamin Wolf <support@eledia.de>
+ * @copyright  2013 eLeDia GmbH
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once('../../config.php');
+require_once('config_usercleanup_form.php');
 
- /// Check for valid admin user - no guest autologin
-    require_login(0, false);
-    $PAGE->set_url('/blocks/eledia_usercleanup/config_usercleanup.php');
-    $PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+// Check for valid admin user - no guest autologin.
+require_login(0, false);
+$PAGE->set_url('/blocks/eledia_usercleanup/config_usercleanup.php');
+$PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+$PAGE->navbar->add(get_string('pluginname', 'block_eledia_usercleanup'));
+$PAGE->set_pagelayout('course');
 
-    $context = get_context_instance(CONTEXT_SYSTEM);
+$context = get_context_instance(CONTEXT_SYSTEM);
 
-    require_capability('moodle/site:config', $context);
+require_capability('moodle/site:config', $context);
 
-//    global $COURSE;
+$mform = new config_usercleanup_form();
 
-    $mform = new config_usercleanup_form();
+if ($mform->is_cancelled()) {
+    redirect($CFG->httpswwwroot);
+}
 
-    if ($mform->is_cancelled()) {
-        redirect($CFG->httpswwwroot);
+$header = get_string('el_header', 'block_eledia_usercleanup');
+$PAGE->set_heading($header);
 
-    } else if ($genparams = $mform->get_data()) {
-//print_object($genparams);
-    }
-
-    $header = get_string('el_header', 'block_eledia_usercleanup');
-    $PAGE->set_heading($header);
-
-    echo $OUTPUT->header();
-    $mform->display();
-    echo $OUTPUT->footer();
-
-?>
+echo $OUTPUT->header();
+$mform->display();
+echo $OUTPUT->footer();
--- blocks/eledia_usercleanup/lang/de/block_eledia_usercleanup.php
+++ blocks/eledia_usercleanup/lang/de/block_eledia_usercleanup.php
@@ -1,5 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * German language file.
+ *
+ * @package    block
+ * @subpackage eledia_usercleanup
+ * @author     Benjamin Wolf <support@eledia.de>
+ * @copyright  2013 eLeDia GmbH
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['back'] = 'zurück';
 
 $string['eledia_usercleanup:addinstance'] = 'Nutzerbereinigungs Block hinzufügen';
@@ -24,9 +47,9 @@ Freundliche Grüße
 ::: Diese Nachricht wurde automatishc erzeugt :::
     ';
 
-$string['pluginname'] = 'eledia Nutzerbereinigung';
+$string['pluginname'] = 'Nutzerbereinigung';
 
-$string['title'] = 'eledia Nutzerbereinigung';
+$string['title'] = 'Nutzerbereinigung';
 
 $string['save_changes'] = 'Änderungen speichern';
 $string['saved'] = 'Änderungen gespeichert';
