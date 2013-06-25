3d6b66fa420201b852ad73bdbb316d232b14be72
Benjamin Wolf <benjamin.wolf@eledia.de>
2013-06-25 10:33:58 +0200

    eledia debug mailstring

--- blocks/eledia_usercleanup/lang/de/block_eledia_usercleanup.php
+++ blocks/eledia_usercleanup/lang/de/block_eledia_usercleanup.php
@@ -46,7 +46,7 @@ Möchten Sie Ihren Account weiterhin nutzen, dann loggen sie sich bitte, innerha
 Freundliche Grüße
 {$a->admin}
 
-::: Diese Nachricht wurde automatishc erzeugt :::
+::: Diese Nachricht wurde automatisch erzeugt :::
     ';
 
 $string['pluginname'] = 'Nutzerbereinigung';
